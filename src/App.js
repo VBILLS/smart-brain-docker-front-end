031a83f2775e8dcb8098c27efeedd970501477d8
Veronica Bills <vbills123@gmail.com>
2019-12-28 20:16:26 -0500

    updating Auth headers

--- src/App.js
+++ src/App.js
@@ -73,7 +73,6 @@ class App extends Component {
           .then(resp => resp.json())
           .then(user => {
             if (user && user.email) {
-              console.log(user)
               this.loadUser(user)
               this.onRouteChange('home');
             }
@@ -98,22 +97,27 @@ class App extends Component {
   };
 
   calculateFaceLocations = data => {
-    return data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box;
+    if (data && data.outputs) {
       const image = document.getElementById('inputimage');
       const width = Number(image.width);
       const height = Number(image.height);
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      };
-    });
+      return data.outputs[0].data.regions.map(face => {
+        const clarifaiFace = face.region_info.bounding_box;
+        return {
+          leftCol: clarifaiFace.left_col * width,
+          topRow: clarifaiFace.top_row * height,
+          rightCol: width - (clarifaiFace.right_col * width),
+          bottomRow: height - (clarifaiFace.bottom_row * height)
+        };
+      });
+    }
+    return
   };
 
   displayFaceBoxes = boxes => {
-    this.setState({ boxes: boxes });
+    if (boxes) {
+      this.setState({ boxes: boxes });
+    }
   };
 
   onInputChange = event => {
@@ -124,7 +128,10 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     fetch('http://localhost:3000/imageurl', {
       method: 'post',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization':  window.sessionStorage.getItem('token') 
+      },
       body: JSON.stringify({
         input: this.state.input
       })
@@ -134,7 +141,10 @@ class App extends Component {
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 
+              'Content-Type': 'application/json',
+              'Authorization':  window.sessionStorage.getItem('token') 
+            },
             body: JSON.stringify({
               id: this.state.user.id
             })
@@ -145,7 +155,6 @@ class App extends Component {
             })
             .catch(console.log);
         }
-        console.log(response);
         this.displayFaceBoxes(this.calculateFaceLocations(response));
       })
       .catch(err => console.log(err));
